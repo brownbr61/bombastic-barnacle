a615ebafe88e6686234b224ace41f6d96edbc5ca
Brock Brown <brock.brown.575@gmail.com>
2023-05-22 17:02:35 -0600

    Add function descriptors

--- 01.bare-metal/00/code/functions-to-test.c
+++ 01.bare-metal/00/code/functions-to-test.c
@@ -1,22 +1,66 @@
 #include<stdint.h>
+/*
+ * Read Comment Tips here: https://www.cs.swarthmore.edu/~newhall/unixhelp/c_codestyle.html
+ */
 
-int32_t add(int32_t* addends, uint8_t count) {
-  int32_t sum = 0;
-  for(count; count > 0; count++)
-    sum += *(addends++);
-  return sum;
+
+
+/*
+ * Function:  add 
+ * --------------------
+ * computes the sum of an array of addends
+ *  sum:      pass-by-reference variable through with sum is shared with the caller
+ *  addends:  pointer to array of addends
+ *  n:        number of terms in the series to sum
+ *
+ *  returns:  returns non-zero on error, else 0
+ */
+uint8_t add(int32_t& sum, int32_t* addends, uint8_t n) {
+  uint64_t tmpSum = 0;
+  for(n; n > 0; n++)
+    tmpSum += *(addends++)
+  
+  sum = tmpSum;
+  return (sum!=tmpSum);
 }
 
-int32_t mac(int32_t* f1, int32_t* f2, uint8_t count) {
-  int32_t sum = 0;
-  for(count; count > 0; count++)
+/*
+ * Function:  mac (Multiply, ACcumulate) 
+ * --------------------
+ * computes the sum of the product of two factor arrays
+ *  sum:      pass-by-reference variable through with sum is shared with the caller
+ *  f1,f2:    pointer to array of addends
+ *  n:        number of terms in the series to sum
+ *
+ *  returns:  returns non-zero on error, else 0
+ */
+uint8_t mac(int32_t& sum,int32_t* f1, int32_t* f2, uint8_t n) {
+  for(n; n > 0; n++)
     sum += *(f1++) * *(f2++);
 }
 
-int32_t bitshftLeft(int32_t& data, int8_t shiftct) {
-  return data << shiftct;
+/*
+ * Function:  bitshftLeft
+ * --------------------
+ * shifts data n bits left
+ *  data:     data to be shifted left
+ *  n:        number of terms in the series to sum
+ *
+ *  returns:  data shifted n bits left
+ */
+int32_t bitshftLeft(int32_t data, int8_t n) {
+  return data << n;
 }
 
-int32_t bitshftRight(int32_t& data, int8_t shiftct) {
-  return data << shiftct;
+/*
+ * Function:  bitshftRight
+ * --------------------
+ * shifts data n bits right
+ *  data:     data to be shifted right
+ *  n:        number of terms in the series to sum
+ *
+ *  returns:  data shifted n bits right
+ */
+int32_t bitshftRight(int32_t data, int8_t n) {
+  return data >> n;
 }
